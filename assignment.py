b58ba4bbe691f112c5e6ab3bf340c390c622c77b
shandilya1998 <shandilya.shreyas@gmail.com>
2019-10-20 17:26:22 +0530

    Histogram equalization tested

--- assignment.py
+++ assignment.py
@@ -10,12 +10,16 @@ class Image():
     def __init__(self, path=False):
         self.path = path
 
-    # This function rescales the image pixel values between 0 to 255
-    def rescale(self):
-        minval = self.image_data.min()
-        maxval = self.image_data.max()
-        rescaled_image = (255/(maxval-minval))*(self.image_data-minval)
-        self.image_data = rescaled_image
+    def rescale(self, im, des_max = 255, des_min = 0):
+        """
+            Rescales the image pixel values between 0 to 255.
+            im - np.ndarray n*m
+            output - np.ndarray n*m
+        """
+        minval = im.min()
+        maxval = im.max()
+        rescaled_image = ((des_max-des_min)/(maxval-minval))*(im-minval+des_min)
+        return rescaled_image
     
     """
     This method is used to import image that are of the type .png
@@ -58,6 +62,10 @@ class Image():
     """
     def histeq(self,im):
         #calculate Histogram
+        #rescale image to have pixel values between 0 to 255
+        minval = im.min()
+        maxval = im.max()
+        im = self.rescale(im)
         orig_hist = self.imhist(im)
         cdf = np.array(self.cumsum(orig_hist)) #cumulative distribution function
         sk = np.uint8(255 * cdf) #finding transfer function values
@@ -69,33 +77,29 @@ class Image():
                 Y[i, j] = sk[int(im[i, j])]
             new_hist = self.imhist(Y)
     	#return transformed image, original and new histogram and transform function
-        return Y,orig_hist,new_hist,sk
+        Y = self.rescale(Y, maxval, minval)
+        return Y,orig_hist,new_hist,sk,maxval,minval
 
-    def main(self):
-        self.image_data = self.image_data.astype('int64')
-        if len(self.image_data.shape)==3:
-            print('3')
-            fig, axs = plt.subplots(self.image_data.shape[2], sharex=True, sharey=True)
-            for i in range(self.image_data.shape[2]):
-                Y, orig_hist, new_hist, sk = self.histeq(self.image_data[:,:,i])
-                axs[i].imshow(Y)
-            plt.show()
-        elif len(self.image_data.shape) == 4:
-            print('4')
-            fig, axs = plt.subplots(self.image_data.shape[2], self.image_data.shape[3], sharex = True, sharey = True)
-            k = 0
-            for i in range(self.image_data.shape[2]):
-                for j in range(self.image_data.shape[3]):
-                        Y, orig_hist, new_hist, sk = self.histeq(self.image_data[:,:,i,j])
-                        axs[k].imshow(Y)
-                        k+=1
-                plt.show()
-
-class gradient():
-    def __init__(self,path = None):
+class Gradient():
+    def __init__(self, path = None):
         self.path = path
-        self.img = nib.load(self.path)
-        self.image_data = self.img.get_fdata()
+    
+    def import_image(self):
+        self.image_data = plt.imread(self.path)
+        
+    """
+        If the image is more than 2 dimensions, 
+        only a slice will be taken for demonstration of histogram equalization
+        and the output image will be grayscale
+    """
+    def import_image_nib_compatible(self):
+        if self.path:
+            img = nib.load(self.path)
+            self.image_data = img.get_fdata()
+        else:
+            example_filename = os.path.join(data_path,'example4d.nii.gz')
+            img = nib.load(example_filename)
+            self.image_data = img.get_fdata()
 
     def grad_x(self, im, formulation):
         """
@@ -112,9 +116,9 @@ class gradient():
                 if formulation != 0:
                     grad_x[i][j] = 0.5*(temp[i][j + 1]-temp[i][j]+temp[i + 1][j + 1]-temp[i + 1][j])
                 else:
-                    grad[i][j] = temp[i + 1][j]-temp[i][j]
+                    grad_x[i][j] = temp[i + 1][j]-temp[i][j]
+        return grad_x
 
-    
     def grad_y(self, im, formulation = 0):
         """
             im - np.ndarray() n*m
@@ -131,6 +135,7 @@ class gradient():
                     grad_y[i][j] = 0.5*(temp[i][j + 1]-temp[i][j]+temp[i + 1][j + 1]-temp[i + 1][j])
                 else:
                     grad_y[i][j] = im[i + 1][j]-im[i][j]
+        return grad_y
 
     def grad_2_x(self, im):
         """
@@ -144,6 +149,7 @@ class gradient():
         for i in range(1, temp.shape[0] - 1):
             for j in range(1, temp.shape[1] - 1):
                 grad_2_x[i][j] = temp[i + 1][j] + temp[i - 1][j] - 2*temp[i][j]
+        return grad_2_x
 
     def grad_2_y(self, im):
         """
@@ -153,10 +159,15 @@ class gradient():
         """
         grad_2_y = np.ndarray(im.shape[0], im.shape[1])
         temp = np.zeros(im.shape + 2, im.shape[1] + 2)
-        temp[1 : temp.shape[0] - 1][1 : temp.shape[1]-]  = im
-            for i in range(1, temp.shape[0]-1):
-                for j in range(1, temp.shape[1]-1):
-                    grad_2_y = temp[i][j + 1] + temp[i][j - 1] - 2*temp[i][j]
+        temp[1 : temp.shape[0] - 1][1 : temp.shape[1] - 1]  = im
+        for i in range(1, temp.shape[0]-1):
+            for j in range(1, temp.shape[1]-1):
+                grad_2_y[i][j] = temp[i][j + 1] + temp[i][j - 1] - 2*temp[i][j]
+        return grad_2_y
+
+    def main(self):
+
+        return True
 
 
 class convolution():
@@ -185,20 +196,72 @@ class convolution():
             filter - np.ndarray() x*y
             Output - (((n - filter.shape[0] + 2*padding)/stride) + 1)*(((m - filter.shape[1] +2*padding)/stride) + 1)
         """
-        y = np.zeros(((im.shape[0] - self.fltr.shape[0] + 2*self.padding)/self.stride)+1, ((im.shape[1] - self.fltr.shape[1] + 2*self.padding)/stride)+1)
+        y = np.zeros(((im.shape[0] - self.fltr.shape[0] + 2*self.padding)/self.stride)+1, 
+                     ((im.shape[1] - self.fltr.shape[1] + 2*self.padding)/self.stride)+1)
         k = 0
         l = 0
-        for i in range(0, ((im.shape[0] - self.fltr.shape[0] + 2*self.padding)/self.stride+1):
+        for i in range(0, ((im.shape[0] - self.fltr.shape[0] + 2*self.padding)/self.stride+1)):
             for j in range(0, ((im.shape[1] - self.fltr.shape[1] + 2*self.padding)/self.stride)+1):
                 y[i][j] = np.dot(im[k : k+self.fltr.shape[0]][l : self.fltr.shape[1]],  self.fltr)
-		l = l + 1
+                l = l + 1
             k = k + 1
         return (y)
 
 def main():
     path = input("enter the path to the image here")
     image = Image(path)
-    if path[-3:]: == 'png':
-        img = Image.import_image()
+    grad = Gradient(path)
+    if path[-3:] == 'png':
+        image.import_image()
+        grad.import_image()
     else:
-        img = Image.import_nib_compatible()
+        image.import_image_nib_compatible()
+        grad.import_image_nib_compatible()
+    image.main()
+    grad.main()
+    image.image_data = image.image_data.astype('int64')
+    if len(image.image_data.shape) == 2:
+        print('2')
+        fig, axs = plt.subplots(2, 2, sharex = True, sharey = True)
+        Y, orig_hist, new_hist, sk, maxval, minval = image.histeq(image.image_data)
+        axs[0, 0].imshow(Y)
+        axs[0, 0].set_title('Transformed Image')
+        axs[0, 1].imshow(image.image_data)
+        axs[0, 1].set_title('Original Image')
+        axs[1, 0].hist(new_hist, bins = np.arange(minval, maxval+1))
+        axs[1, 0].set_title('Transformed Histogram')
+        axs[1, 1].hist(orig_hist, bins = np.arange(minval, maxval+1))
+        axs[1, 1].set_title('Original Histogram')
+        plt.show()
+    elif len(image.image_data.shape)==3:
+        print('3')
+        for i in range(image.image_data.shape[2]):
+            fig, axs = plt.subplots(2,2, sharex = True, sharey = True)
+            Y, orig_hist, new_hist, sk, maxval, minval = image.histeq(image.image_data[:,:,i])
+            axs[0, 0].imshow(Y)
+            axs[0, 0].set_title('Transformed Image')
+            axs[0, 1].imshow(image.image_data[:, :, i])
+            axs[0, 1].set_title('Original Image')
+            axs[1, 0].hist(new_hist, bins = np.arange(minval, maxval+1))
+            axs[1, 0].set_title('Transformed Histogram')
+            axs[1, 1].hist(orig_hist, bins = np.arange(minval, maxval+1))
+            axs[1, 1].set_title('Original Histogram')
+            plt.show()
+    elif len(image.image_data.shape) == 4:
+        print('4')
+        for i in range(image.image_data.shape[2]):
+            for j in range(image.image_data.shape[3]):
+                fig, axs = plt.subplots(2,2, sharex = True, sharey = True)
+                Y, orig_hist, new_hist, sk, maxval, minval = image.histeq(image.image_data[:,:,i,j])
+                axs[0, 0].imshow(Y)
+                axs[0, 0].set_title('Transformed Image')
+                axs[0, 1].imshow(image.image_data[:, :, i, j])
+                axs[0, 1].set_title('Original Image')
+                axs[1, 0].hist(new_hist, bins = np.arange(minval, maxval+1))
+                axs[1, 0].set_title('Transformed Histogram')
+                axs[1, 1].hist(orig_hist, bins = np.arange(minval, maxval+1))
+                axs[1, 1].set_title('Original Histogram')
+                plt.show()
+
+
+main()
